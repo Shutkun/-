598fc48b46af587214a8b19157831fe097203111
omega <omega@VOLHOVOI>
2023-09-20 11:06:49 +0300

    Добавьте файлы проекта.

---
+++ Учебный/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Учебный
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            int age;
+            int programCount;
+            int money;
+            int time;
+            bool truth;
+
+            name = "Михаил";
+            age = 33;
+            programCount = 2;
+            money = 100;
+            time = 50;
+            truth = true;
+
+            Console.WriteLine("Здравствуйте, меня зовут " + name + ".");
+            Console.WriteLine("Мне " + age + " лет.");
+            Console.WriteLine("Я хочу научиться делать игры, это моя " + programCount + " программа на языке C#.");
+            Console.WriteLine("Когда я закончу обучение я хочу зарабатывать " + money + " денег в " + time + " наносекунд.");
+            Console.WriteLine("Достигну ли я желаемого? " + truth + "!");
+        }
+    }
+}
